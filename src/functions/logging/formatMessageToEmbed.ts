a0062c3f4664340f2596dbcadbd6afdcf104262f
Jhan Gale Garcia <garcia.johngale@gmail.com>
2023-11-06 20:04:13 +0800

    chore: deps
    * add(logging): formatMessagesToAttachment
    
    * add(logging): formatMessageToEmbed

---
+++ src/functions/logging/formatMessageToEmbed.ts
@@ -0,0 +1,34 @@
+import type { Message } from "discord.js";
+import i18next from "i18next";
+import { truncateEmbed } from "../../util/embed.js";
+import { Color } from "../../Constants.js";
+
+export function formatMessageToEmbed(message: Message<true>, locale: string) {
+	let embed = truncateEmbed({
+		author: {
+			name: `${message.author.tag} (${message.author.id})`,
+			icon_url: message.author.displayAvatarURL(),
+		},
+		description: message.content.length ? message.content : i18next.t("common.errors.no_content", { lng: locale }),
+		timestamp: message.createdAt.toISOString(),
+		footer: {
+			text: `#${message.channel.name}`,
+		},
+		color: Color.DiscordEmbedBackground,
+	});
+
+	const attachment = message.attachments.first();
+	const attachmentIsImage = ["image/jpeg", "image/png", "image/gif"].includes(attachment?.contentType ?? "");
+	const attachmentIsImageNaive = [".jpg", ".png", ".gif"].some((ext) => attachment?.name.endsWith(ext));
+
+	if (attachment && (attachmentIsImage || attachmentIsImageNaive)) {
+		embed = {
+			...embed,
+			image: {
+				url: attachment.url,
+			},
+		};
+	}
+
+	return embed;
+}
---
+++ src/functions/logging/formatMessagesToAttachment.ts
@@ -0,0 +1,94 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc.js";
+import {
+	type Collection,
+	type Message,
+	messageLink,
+	MessageType,
+	type PartialMessage,
+	type Snowflake,
+} from "discord.js";
+import i18next from "i18next";
+import kleur from "kleur";
+import { DATE_FORMAT_WITH_SECONDS } from "../../Constants.js";
+
+kleur.enabled = true;
+
+dayjs.extend(utc);
+
+export function formatMessagesToAttachment(
+	messages: Collection<Snowflake, Message | PartialMessage>,
+	locale: string,
+	primaryHighlightMessageIds: Snowflake[] = [],
+	secondaryHighlightMessageIds: Snowflake[] = [],
+) {
+	return messages
+		.map((message) => {
+			const isPrimaryHighlight = primaryHighlightMessageIds.includes(message.id);
+			const isSecondaryHighlight = secondaryHighlightMessageIds.includes(message.id);
+			const outParts = [
+				`[${dayjs(message.createdTimestamp).utc().format(DATE_FORMAT_WITH_SECONDS)} (UTC)] ${
+					message.author?.tag ?? "Unknown author"
+				} (${message.author?.id ?? "Unknown author"}): ${
+					message.cleanContent ? message.cleanContent.replaceAll("\n", "\n") : ""
+				}`,
+			];
+
+			if (message.attachments.size) {
+				outParts.push(
+					message.attachments
+						.map((attachment) =>
+							i18next.t("log.guild_log.message_bulk_deleted.attachment", {
+								url: attachment.proxyURL,
+								lng: locale,
+							}),
+						)
+						.join("\n"),
+				);
+			}
+
+			if (message.stickers.size) {
+				outParts.push(
+					message.stickers
+						.map((sticker) =>
+							i18next.t("log.guild_log.message_bulk_deleted.sticker", {
+								name: sticker.name,
+								lng: locale,
+							}),
+						)
+						.join("\n"),
+				);
+			}
+
+			if (message.type === MessageType.Reply && message.reference && message.mentions.repliedUser) {
+				const { guildId, channelId, messageId } = message.reference;
+				const replyURL = messageLink(channelId, messageId!, guildId!);
+
+				outParts.push(
+					message.mentions.users.has(message.mentions.repliedUser.id)
+						? i18next.t("log.guild_log.message_bulk_deleted.reply_to_mentions", {
+								message_id: messageId,
+								message_url: replyURL,
+								user_tag: message.mentions.repliedUser.tag,
+								user_id: message.mentions.repliedUser.id,
+								lng: locale,
+						  })
+						: i18next.t("log.guild_log.message_bulk_deleted.reply_to", {
+								message_id: messageId,
+								message_url: replyURL,
+								user_tag: message.mentions.repliedUser.tag,
+								user_id: message.mentions.repliedUser.id,
+								lng: locale,
+						  }),
+				);
+			}
+
+			const outSection = outParts.join("\n");
+			return isPrimaryHighlight
+				? kleur.yellow(outSection)
+				: isSecondaryHighlight
+				? kleur.white(outSection)
+				: outSection;
+		})
+		.join("\n");
+}
