4836be00d2300c8d8099a45c98f68051f7c2c46a
Jhan Gale Garcia <garcia.johngale@gmail.com>
2023-11-16 11:47:44 +0800

    feat: status-report change log event

---
+++ src/events/manual-report-status/threadUpdate.ts
@@ -0,0 +1,80 @@
+import { on } from "node:events";
+import { setTimeout as pSetTimeout } from "node:timers/promises";
+import { Client, Events, AuditLogEvent, type ThreadChannel } from "discord.js";
+import type { Sql } from "postgres";
+import type { Event } from "../../Event.js";
+import { inject, injectable } from "tsyringe";
+import { AUDIT_LOG_WAIT_SECONDS } from "../../Constants.js";
+import { upsertReportLog } from "../../functions/logging/upsertReportLog.js";
+import { type RawReport, transformReport } from "../../functions/reports/transformReport.js";
+import { updateReport } from "../../functions/reports/updateReport.js";
+import { type ReportStatusTagTuple, getGuildSetting, SettingsKeys } from "../../functions/settings/getGuildSetting.js";
+import { arrayEquals } from "../../util/arrayEquals.js";
+import { kSQL } from "../../tokens.js";
+import { logger } from "../../logger.js";
+
+@injectable()
+export default class implements Event {
+	public name = "Manual report status change";
+
+	public event = Events.ThreadUpdate as const;
+
+	public constructor(
+		public readonly client: Client<true>,
+		@inject(kSQL) public readonly sql: Sql<any>,
+	) {}
+
+	public async execute(): Promise<void> {
+		for await (const [oldPost, newPost] of on(this.client, this.event) as AsyncIterableIterator<
+			[ThreadChannel, ThreadChannel]
+		>) {
+			try {
+				const reportStatusTags = await getGuildSetting<ReportStatusTagTuple>(
+					oldPost.guildId,
+					SettingsKeys.ReportStatusTags,
+				);
+
+				const oldStatusTags = oldPost.appliedTags.filter((tag) => reportStatusTags.includes(tag));
+				const newStatusTags = newPost.appliedTags.filter((tag) => reportStatusTags.includes(tag));
+
+				if (oldStatusTags.length !== 1 && newStatusTags.length === 1) {
+					const [rawReport] = await this.sql<[RawReport]>`
+                              select *
+                              from reports
+                              where guild_id = ${oldPost.guildId}
+                                   and log_post_id = ${oldPost.id}
+                         `;
+
+					if (rawReport) {
+						await pSetTimeout(AUDIT_LOG_WAIT_SECONDS * 1_000);
+						const auditLogs = await oldPost.guild.fetchAuditLogs({ limit: 10, type: AuditLogEvent.ThreadUpdate });
+						const auditLog = auditLogs.entries.find(
+							(entry) =>
+								entry.target.id === oldPost.id &&
+								arrayEquals(entry.changes[0]?.new as string[] | undefined, newPost.appliedTags) &&
+								arrayEquals(entry.changes[0]?.old as string[] | undefined, oldPost.appliedTags),
+						);
+
+						const [statusTag] = newStatusTags;
+						const oldReport = transformReport(rawReport);
+						const reportStatusTag = reportStatusTags[oldReport.status];
+
+						if (statusTag && statusTag !== reportStatusTag) {
+							const report = await updateReport(
+								{
+									...oldReport,
+									status: reportStatusTag.indexOf(statusTag),
+								},
+								auditLog?.executor ?? undefined,
+							);
+							await upsertReportLog(oldPost.guild, report, undefined);
+						}
+					}
+				}
+			} catch (error_) {
+				const error = error_ as Error;
+				logger.error(error, error.message);
+			}
+		}
+	}
+}
