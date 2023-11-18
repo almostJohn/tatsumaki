fdd2440b9f0dfbbcb7b32b925842af30b69bdb7a
Jhan Gale Garcia <garcia.johngale@gmail.com>
2023-11-18 19:54:35 +0800

    add: test ping command

---
+++ src/commands/utility/ping.ts
@@ -0,0 +1,18 @@
+import { Command } from "../../Command.js";
+import type { ArgsParam, InteractionParam, LocaleParam } from "../../interactions/Interaction.js";
+import i18next from "i18next";
+import type { PingCommand } from "../../interactions/index.js";
+
+export default class extends Command<typeof PingCommand> {
+	public override async chatInput(
+		interaction: InteractionParam,
+		args: ArgsParam<typeof PingCommand>,
+		locale: LocaleParam,
+	): Promise<void> {
+		await interaction.deferReply({ ephemeral: args.hide ?? true });
+
+		await interaction.editReply({
+			content: i18next.t("command.utility.ping.success", { lng: locale }),
+		});
+	}
+}
