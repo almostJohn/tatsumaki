486063add89119eff61db3e7ae152e45f77e310f
Jhan Gale Garcia <garcia.johngale@gmail.com>
2023-11-26 19:57:55 +0800

    feat: ban command

---
+++ src/commands/moderation/ban.ts
@@ -0,0 +1,160 @@
+import {
+	Command,
+	kRedis,
+	logger,
+	createButton,
+	createMessageActionRow,
+	truncateEmbed,
+	inject,
+	injectable,
+} from "@almostjohn/djs-framework";
+import type { ArgsParam, InteractionParam, LocaleParam } from "@almostjohn/djs-framework";
+import { ButtonStyle, ComponentType } from "discord.js";
+import i18next from "i18next";
+import type { Redis } from "ioredis";
+import { nanoid } from "nanoid";
+import { CASE_REASON_MAX_LENGTH } from "../../Constants.js";
+import { CaseAction, createCase } from "../../functions/cases/createCase.js";
+import { generateCasePayload } from "../../functions/logging/generateCasePayload.js";
+import { upsertCaseLog } from "../../functions/logging/upsertCaseLog.js";
+import { checkLogChannel } from "../../functions/settings/checkLogChannel.js";
+import { SettingsKeys, getGuildSetting } from "../../functions/settings/getGuildSetting.js";
+import type { BanCommand } from "../../interactions/index.js";
+import { generateHistory } from "../../util/generateHistory.js";
+
+@injectable()
+export default class extends Command<typeof BanCommand> {
+	public constructor(@inject(kRedis) public readonly redis: Redis) {
+		super();
+	}
+
+	public override async chatInput(
+		interaction: InteractionParam,
+		args: ArgsParam<typeof BanCommand>,
+		locale: LocaleParam,
+	): Promise<void> {
+		const reply = await interaction.deferReply({ ephemeral: true });
+
+		const modLogChannel = checkLogChannel(
+			interaction.guild,
+			await getGuildSetting(interaction.guildId, SettingsKeys.ModLogChannelId),
+		);
+
+		if (!modLogChannel) {
+			throw new Error(i18next.t("common.errors.no_mod_log_channel", { lng: locale }));
+		}
+
+		let alreadyBanned = false;
+		try {
+			await interaction.guild.bans.fetch(args.user.user.id);
+			alreadyBanned = true;
+		} catch {}
+
+		if (alreadyBanned) {
+			throw new Error(
+				i18next.t("command.mod.ban.errors.already_banned", {
+					user: `${args.user.user.toString()} - ${args.user.user.tag} (${args.user.user.id})`,
+					lng: locale,
+				}),
+			);
+		}
+
+		if (args.user.member && !args.user.member.bannable) {
+			throw new Error(
+				i18next.t("command.mod.ban.errors.missing_permissions", {
+					user: `${args.user.user.toString()} - ${args.user.user.tag} (${args.user.user.id})`,
+					lng: locale,
+				}),
+			);
+		}
+
+		if (args.reason && args.reason.length >= CASE_REASON_MAX_LENGTH) {
+			throw new Error(
+				i18next.t("command.mod.common.errors.max_length_reason", {
+					reason_max_length: CASE_REASON_MAX_LENGTH,
+					lng: locale,
+				}),
+			);
+		}
+
+		const banKey = nanoid();
+		const cancelKey = nanoid();
+
+		const banButton = createButton({
+			label: i18next.t("command.mod.ban.buttons.execute", { lng: locale }),
+			customId: banKey,
+			style: ButtonStyle.Danger,
+		});
+		const cancelButton = createButton({
+			label: i18next.t("command.common.buttons.cancel", { lng: locale }),
+			customId: cancelKey,
+			style: ButtonStyle.Secondary,
+		});
+
+		const embed = truncateEmbed(await generateHistory(interaction, args.user, locale!));
+
+		await interaction.editReply({
+			content: i18next.t("command.mod.ban.pending", {
+				user: `${args.user.user.toString()} - ${args.user.user.tag} (${args.user.user.id})`,
+				lng: locale,
+			}),
+			embeds: [embed],
+			components: [createMessageActionRow([cancelButton, banButton])],
+		});
+
+		const collectedInteraction = await reply
+			.awaitMessageComponent({
+				filter: (collected) => collected.user.id === interaction.user.id,
+				componentType: ComponentType.Button,
+				time: 15_000,
+			})
+			.catch(async () => {
+				try {
+					await interaction.editReply({
+						content: i18next.t("command.common.errors.timed_out", { lng: locale }),
+						components: [],
+					});
+				} catch (error_) {
+					const error = error_ as Error;
+					logger.error(error, error.message);
+				}
+
+				return undefined;
+			});
+
+		if (collectedInteraction?.customId === cancelKey) {
+			await collectedInteraction.update({
+				content: i18next.t("command.mod.ban.cancel", {
+					user: `${args.user.user.toString()} - ${args.user.user.tag} (${args.user.user.id})`,
+					lng: locale,
+				}),
+				components: [],
+			});
+		} else if (collectedInteraction?.customId === banKey) {
+			await collectedInteraction.deferUpdate();
+
+			await this.redis.setex(`guild:${collectedInteraction.guildId}:user:${args.user.user.id}:ban`, 15, "");
+			const case_ = await createCase(
+				interaction.guild,
+				generateCasePayload({
+					guildId: collectedInteraction.guildId,
+					user: collectedInteraction.user,
+					args: {
+						...args,
+						days: Math.min(Math.max(Number(args.days ?? 0), 0), 7),
+					},
+					action: CaseAction.Ban,
+				}),
+			);
+			await upsertCaseLog(collectedInteraction.guild, collectedInteraction.user, case_);
+
+			await collectedInteraction.editReply({
+				content: i18next.t("command.mod.ban.success", {
+					user: `${args.user.user.toString()} - ${args.user.user.tag} (${args.user.user.id})`,
+					lng: locale,
+				}),
+				components: [],
+			});
+		}
+	}
+}
