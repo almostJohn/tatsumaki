7fbd640ae0eb7fd7690015165620d0d1461eccba
Jhan Gale Garcia <garcia.johngale@gmail.com>
2023-12-10 13:47:21 +0800

    feat(commands): softban & duration

---
+++ src/commands/moderation/duration.ts
@@ -0,0 +1,100 @@
+import { ms } from "@naval-base/ms";
+import { Command, kRedis, inject, injectable } from "@almostjohn/djs-framework";
+import type { ArgsParam, InteractionParam, LocaleParam } from "@almostjohn/djs-framework";
+import { hyperlink, messageLink } from "discord.js";
+import type { Redis } from "ioredis";
+import i18next from "i18next";
+import { CaseAction } from "../../functions/cases/createCase.js";
+import { getCase } from "../../functions/cases/getCase.js";
+import { updateCase } from "../../functions/cases/updateCase.js";
+import { upsertCaseLog } from "../../functions/logging/upsertCaseLog.js";
+import { checkLogChannel } from "../../functions/settings/checkLogChannel.js";
+import { getGuildSetting, SettingsKeys } from "../../functions/settings/getGuildSetting.js";
+import type { DurationCommand } from "../../interactions/index.js";
+
+@injectable()
+export default class extends Command<typeof DurationCommand> {
+	public constructor(@inject(kRedis) public readonly redis: Redis) {
+		super();
+	}
+
+	public override async chatInput(
+		interaction: InteractionParam,
+		args: ArgsParam<typeof DurationCommand>,
+		locale: LocaleParam,
+	): Promise<void> {
+		await interaction.deferReply({ ephemeral: true });
+
+		const modLogChannel = checkLogChannel(
+			interaction.guild,
+			await getGuildSetting(interaction.guildId, SettingsKeys.ModLogChannelId),
+		);
+
+		if (!modLogChannel) {
+			throw new Error(i18next.t("common.errors.no_mod_log_channel", { lng: locale }));
+		}
+
+		const originalCase = await getCase(interaction.guildId, args.case);
+
+		if (!originalCase) {
+			throw new Error(i18next.t("command.mod.common.errors.no_case", { case: args.case, lng: locale }));
+		}
+
+		if (originalCase.actionProcessed) {
+			const user = await interaction.client.users.fetch(originalCase.targetId);
+			throw new Error(
+				i18next.t("command.mod.common.errors.already_processed", {
+					user: `${user.toString()} - ${user.tag} (${user.id})`,
+					case: hyperlink(
+						`#${originalCase.caseId}`,
+						messageLink(modLogChannel.id, originalCase.logMessageId!, interaction.guildId),
+					),
+					lng: locale,
+				}),
+			);
+		}
+
+		const parsedDuration = ms(args.duration);
+
+		if (parsedDuration < 300_000 || parsedDuration > 2_419_200_000 || Number.isNaN(parsedDuration)) {
+			throw new Error(i18next.t("command.common.errors.duration_format", { lng: locale }));
+		}
+
+		const actionExpiration = Date.now() + Number(parsedDuration);
+
+		if (originalCase.action === CaseAction.Timeout) {
+			try {
+				const member = await interaction.guild.members.fetch(originalCase.targetId);
+				await this.redis.setex(`guild:${interaction.guild.id}:user:${member.user.id}:timeout`, 15, "");
+				await member.disableCommunicationUntil(actionExpiration);
+			} catch {
+				throw new Error(
+					i18next.t("command.mod.duration.errors.timeout", {
+						case: hyperlink(
+							`#${originalCase.caseId}`,
+							messageLink(modLogChannel.id, originalCase.logMessageId!, interaction.guildId),
+						),
+						lng: locale,
+					}),
+				);
+			}
+		}
+
+		const case_ = await updateCase({
+			caseId: originalCase.caseId,
+			guildId: interaction.guildId,
+			actionExpiration: new Date(actionExpiration),
+		});
+		await upsertCaseLog(interaction.guild, interaction.user, case_);
+
+		await interaction.editReply({
+			content: i18next.t("command.mod.duration.success", {
+				case: hyperlink(
+					`#${originalCase.caseId}`,
+					messageLink(modLogChannel.id, originalCase.logMessageId!, interaction.guildId),
+				),
+				lng: locale,
+			}),
+		});
+	}
+}
---
+++ src/commands/moderation/softban.ts
@@ -0,0 +1,165 @@
+import {
+	Command,
+	kRedis,
+	logger,
+	createButton,
+	createMessageActionRow,
+	truncateEmbed,
+	inject,
+	injectable,
+} from "@almostjohn/djs-framework";
+import type { ArgsParam, InteractionParam, LocaleParam } from "@almostjohn/djs-framework";
+import { ButtonStyle, ComponentType } from "discord.js";
+import i18next from "i18next";
+import type { Redis } from "ioredis";
+import { nanoid } from "nanoid";
+import { CASE_REASON_MAX_LENGTH } from "../../Constants.js";
+import { CaseAction, createCase } from "../../functions/cases/createCase.js";
+import { generateCasePayload } from "../../functions/logging/generateCasePayload.js";
+import { upsertCaseLog } from "../../functions/logging/upsertCaseLog.js";
+import { checkLogChannel } from "../../functions/settings/checkLogChannel.js";
+import { getGuildSetting, SettingsKeys } from "../../functions/settings/getGuildSetting.js";
+import type { SoftbanCommand } from "../../interactions/index.js";
+import { generateHistory } from "../../util/generateHistory.js";
+
+@injectable()
+export default class extends Command<typeof SoftbanCommand> {
+	public constructor(@inject(kRedis) public readonly redis: Redis) {
+		super();
+	}
+
+	public override async chatInput(
+		interaction: InteractionParam,
+		args: ArgsParam<typeof SoftbanCommand>,
+		locale: LocaleParam,
+	): Promise<void> {
+		const reply = await interaction.deferReply({ ephemeral: true });
+
+		const modLogChannel = checkLogChannel(
+			interaction.guild,
+			await getGuildSetting(interaction.guildId, SettingsKeys.ModLogChannelId),
+		);
+
+		if (!modLogChannel) {
+			throw new Error(i18next.t("common.errors.no_mod_log_channel", { lng: locale }));
+		}
+
+		if (args.user.member && !args.user.member.bannable) {
+			throw new Error(
+				i18next.t("command.mod.softban.errors.missing_permissions", {
+					user: `${args.user.user.toString()} - ${args.user.user.tag} (${args.user.user.id})`,
+					lng: locale,
+				}),
+			);
+		}
+
+		if (args.reason && args.reason.length >= CASE_REASON_MAX_LENGTH) {
+			throw new Error(
+				i18next.t("command.mod.common.errors.max_length_reason", {
+					reason_max_length: CASE_REASON_MAX_LENGTH,
+					lng: locale,
+				}),
+			);
+		}
+
+		const isStillMember = interaction.guild.members.resolve(args.user.user.id);
+
+		const softbanKey = nanoid();
+		const cancelKey = nanoid();
+
+		const softbanButton = createButton({
+			label: i18next.t("command.mod.softban.buttons.execute", { lng: locale }),
+			customId: softbanKey,
+			style: ButtonStyle.Danger,
+		});
+		const cancelButton = createButton({
+			label: i18next.t("command.common.buttons.cancel", { lng: locale }),
+			customId: cancelKey,
+			style: ButtonStyle.Secondary,
+		});
+
+		const embeds = isStillMember ? [truncateEmbed(await generateHistory(interaction, args.user, locale!))] : [];
+
+		await interaction.editReply({
+			content: i18next.t(isStillMember ? "command.mod.softban.pending" : "command.mod.softban.not_member", {
+				user: `${args.user.user.toString()} - ${args.user.user.tag} (${args.user.user.id})`,
+				lng: locale,
+			}),
+			embeds,
+			components: [createMessageActionRow([cancelButton, softbanButton])],
+		});
+
+		const collectedInteraction = await reply
+			.awaitMessageComponent({
+				filter: (collected) => collected.user.id === interaction.user.id,
+				componentType: ComponentType.Button,
+				time: 15_000,
+			})
+			.catch(async () => {
+				try {
+					await interaction.editReply({
+						content: i18next.t("command.common.errors.timed_out", { lng: locale }),
+						components: [],
+					});
+				} catch (error_) {
+					const error = error_ as Error;
+					logger.error(error, error.message);
+				}
+
+				return undefined;
+			});
+
+		if (collectedInteraction?.customId === cancelKey) {
+			await collectedInteraction.update({
+				content: i18next.t("command.mod.softban.cancel", {
+					user: `${args.user.user.toString()} - ${args.user.user.tag} (${args.user.user.id})`,
+					lng: locale,
+				}),
+				components: [],
+			});
+		} else if (collectedInteraction?.customId === softbanKey) {
+			await collectedInteraction.deferUpdate();
+
+			await this.redis.setex(`guild:${collectedInteraction.guildId}:user:${args.user.user.id}:ban`, 15, "");
+			await this.redis.setex(`guild:${collectedInteraction.guildId}:user:${args.user.user.id}:unban`, 15, "");
+
+			if (isStillMember) {
+				const case_ = await createCase(
+					collectedInteraction.guild,
+					generateCasePayload({
+						guildId: collectedInteraction.guildId,
+						user: collectedInteraction.user,
+						args: {
+							...args,
+							days: Math.min(Math.max(Number(args.days ?? 1), 0), 7),
+						},
+						action: CaseAction.Softban,
+					}),
+				);
+				await upsertCaseLog(collectedInteraction.guild, collectedInteraction.user, case_);
+			} else {
+				const reason = i18next.t("command.mod.softban.reasons.clear_messages", {
+					user: collectedInteraction.user.tag,
+					lng: locale,
+				});
+
+				await interaction.guild.bans.create(args.user.user, {
+					reason,
+					deleteMessageSeconds: Math.min(Math.max(Number(args.days ?? 1), 0), 7),
+				});
+				await interaction.guild.bans.remove(args.user.user, reason);
+			}
+
+			await collectedInteraction.editReply({
+				content: i18next.t(
+					isStillMember ? "command.mod.softban.success.regular" : "command.mod.softban.success.clear_messages",
+					{
+						user: `${args.user.user.toString()} - ${args.user.user.tag} (${args.user.user.id})`,
+						lng: locale,
+					},
+				),
+				components: [],
+			});
+		}
+	}
+}
