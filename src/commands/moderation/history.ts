3cb38521b05fe1b2f824477a8aced405a237e8e3
Jhan Gale Garcia <garcia.johngale@gmail.com>
2023-12-31 06:56:18 +0800

    feat: history command

---
+++ src/commands/moderation/history.ts
@@ -0,0 +1,59 @@
+import { Command, truncateEmbed } from "@almostjohn/djs-framework";
+import type { ArgsParam, InteractionParam, LocaleParam, CommandMethod } from "@almostjohn/djs-framework";
+import type { HistoryCommand, HistoryUserContextCommand } from "../../interactions/index.js";
+import { HistoryType, generateHistory } from "../../util/generateHistory.js";
+
+type HistoryCommandArgs =
+	| ArgsParam<typeof HistoryCommand>["cases"]
+	| ArgsParam<typeof HistoryCommand>["reports"]
+	| ArgsParam<typeof HistoryUserContextCommand>;
+export default class extends Command<typeof HistoryCommand | typeof HistoryUserContextCommand> {
+	public constructor() {
+		super(["history", "History"]);
+	}
+
+	private async handle(
+		interaction: InteractionParam | InteractionParam<CommandMethod.UserContext>,
+		args: HistoryCommandArgs,
+		type: HistoryType,
+		locale: LocaleParam,
+	): Promise<void> {
+		const embed = truncateEmbed(await generateHistory(interaction, args.user, locale!, type));
+
+		await interaction.editReply({
+			embeds: [embed],
+		});
+	}
+
+	public override async chatInput(
+		interaction: InteractionParam,
+		args: ArgsParam<typeof HistoryCommand>,
+		locale: LocaleParam,
+	): Promise<void> {
+		await interaction.deferReply({ ephemeral: args.cases?.hide ?? args.reports?.hide ?? true });
+
+		switch (Object.keys(args)[0]) {
+			case "cases": {
+				await this.handle(interaction, args.cases, HistoryType.Case, locale);
+				break;
+			}
+
+			case "reports": {
+				await this.handle(interaction, args.reports, HistoryType.Report, locale);
+				break;
+			}
+
+			default:
+				break;
+		}
+	}
+
+	public override async userContext(
+		interaction: InteractionParam<CommandMethod.UserContext>,
+		args: ArgsParam<typeof HistoryUserContextCommand>,
+		locale: LocaleParam,
+	): Promise<void> {
+		await interaction.deferReply({ ephemeral: true });
+		await this.handle(interaction, args, HistoryType.Case, locale);
+	}
+}
